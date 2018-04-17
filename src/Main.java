890c90c6cdc05570535cc33307e9be2e84f366d0
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-04-17 15:08:22 -0600

    Haciendo cambios

--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,6 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  * @author Raul Monzon					17014
  * Clase principal
  */
-
-
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Scanner teclado = new Scanner(System.in);
